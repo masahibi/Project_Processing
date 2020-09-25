9db01377649cc578701d38909f6706868b88cf9b
masahibi <masahibi13@gmail.com>
2020-09-25 18:38:46 +0900

    Initial commit

---
+++ src/Project/Main.java
@@ -0,0 +1,24 @@
+package Project;
+
+import processing.core.*;
+
+public class Main extends PApplet {
+
+    @Override
+    public void settings() {
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    public static void main(String[] args){
+        PApplet.main("Project.Main");
+    }
+}
